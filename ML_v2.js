a740284e39f862edb3b604d4806ec308db790414
yuridasilva1704 <51483291+yuridasilva1704@users.noreply.github.com>
2021-08-03 15:57:13 -0300

    Update ML_v2.js

--- ML_v2.js
+++ ML_v2.js
@@ -1,7 +1,20 @@
 function calculadora() {
+	
+// Para uma pontução entre 3800 Pontos e 3849 Pontos	
+if (pontos >= 3800 && pontos < 3850) {
+	
+	var mvp1 = 15
+	var ouro1 = 10.5
+	var prata1 = 7
+	var bronze1 = 4
+	var media1 = 9.125
+
+	var dif1 = 3849-pontos
+	var res1 = dif1/media1
+	alert("Restam " + parseInt(Math. ceil(res1)) + " partidas em média para o top supremo."
 
 
-// Para uma pontução entre 3850 Pontos e 3900 Pontos	
+// Para uma pontução entre 3850 Pontos e 3899 Pontos	
 if (pontos >= 3850 && pontos < 3900) {
 	
 	var mvp2 = 11.8 
@@ -9,13 +22,12 @@ if (pontos >= 3850 && pontos < 3900) {
 	var prata2 = 5
 	var bronze2 = 4
 	var media2 = 7.325
-	
-	var media2 = 7.325
 
 	var dif2 = 3899-pontos
 	var res2 = dif2/media2
-	alert(res2)
-
-
+	alert("Restam " + parseInt(Math. ceil(res2)) + " partidas em média para o top supremo.")
+		
+	}
 }
+
 	}
