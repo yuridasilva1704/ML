155aa506a9ec60b0a99b4a7814a91fd1afe020f3
yuridasilva1704 <51483291+yuridasilva1704@users.noreply.github.com>
2021-08-03 16:27:09 -0300

    Create Rascunho.py

---
+++ Rascunho.py
@@ -0,0 +1,61 @@
+<script>
+pontos = prompt("")
+
+if (pontos >= 3750 && pontos <= 4100) {
+
+
+	if (pontos >= 3800 && pontos <= 4100) {
+    	var mvp1 = 15
+		var ouro1 = 10.5
+		var prata1 = 7
+		var bronze1 = 4
+		var media1 = 9.125
+
+		var dif1 = 3849-pontos
+		var res1 = dif1/media1
+		alert(res1)
+
+    	if (pontos >= 3850 && pontos <= 4100) {
+        
+    		var mvp2 = 11.8 
+    		var ouro2 = 8.5
+    		var prata2 = 5
+    		var bronze2 = 4
+    		var media2 = 7.325
+
+    		var dif2 = 3899-pontos
+        	var res2 = dif2/media2
+    		alert(res2)
+	
+        
+            	if (pontos >= 3900 && pontos <= 4100) {
+
+                
+                	if (pontos >= 3950 && pontos <= 4100) {
+
+                    
+                    	if (pontos >= 3950 && pontos <= 4100) {
+
+                        
+                        	if (pontos >= 4000 && pontos <= 4100) {
+
+                            
+                            	if (pontos >= 4000 && pontos <= 4100) {
+
+                                
+                                	if (pontos >= 4050 && pontos <= 4100) {
+
+                                   
+                            	}
+    						}
+    					}
+    				}
+    			}
+    		}
+    	}
+	}
+}
+
+
+
+</script>
